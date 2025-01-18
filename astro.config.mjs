55ed714eb6a3f524d56f4b7016033ac43ff63d33
coilt <cameraboi@gmail.com>
2025-01-19 01:22:19 +0700

    Add fixed video thumbnails on mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import tunnel from 'astro-tunnel';
 import lenis from "astro-lenis";
 
 export default defineConfig({
-  integrations: [tailwind(), lenis()],
+  integrations: [tailwind(), lenis(), tunnel()],
   vite: {
     build: {
       rollupOptions: {
